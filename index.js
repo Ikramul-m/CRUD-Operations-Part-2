58b6d129c63ac0536b1474acc922c1012a792d1f
Ikramul-m <i20727172@gmail.com>
2024-05-09 23:08:13 +0530

    .

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 8000;
 const hostName = "localhost";
 
 app.listen(PORT, () => {
@@ -14,6 +15,21 @@ app.get("/", (req, res) => {
   res.send("Welcome to my Home!!!");
 });
 
+app.get('/api/MyFamily', async (req,res) => {
+    try
+    {
+        const member = await member.find({})
+        res.status(200).json(member)
+    }
+    catch(error) 
+    {
+        res.status(404).json({message: error.message})
+    }
+})
+
+
+
+
 app.post("/api/MyFamily", async (req, res) => {
   try {
     const member = await member.create(req.body);
@@ -22,3 +38,10 @@ app.post("/api/MyFamily", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/MyFamily").then(() => {
+    console.log('Connected to database.')
+}).catch(() => {
+    console.log('COnnection failed.')
+})
