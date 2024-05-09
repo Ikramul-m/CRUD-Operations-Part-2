35ec48c89d3c375279413e5552589027206f5abd
Ikramul-m <i20727172@gmail.com>
2024-05-09 22:56:38 +0530

    .

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ app.listen(PORT, () => {
 app.get("/", (req, res) => {
   res.send("Welcome to my Home!!!");
 });
+
+app.post("/api/MyFamily", async (req, res) => {
+  try {
+    const member = await member.create(req.body);
+    res.status(200).json(member);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
